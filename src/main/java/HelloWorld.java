be92705f7f102e3521e3362787bf322b95d7353f
Lucas Pereira <2231100@my.ipleiria.pt>
2025-03-05 10:21:16 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,12 +2,15 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld(){
         setContentPane(panel1);
         pack();
+        lblMensagem2.setText("O Lucas esteve aqui");
     }
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
+
     }
 }
