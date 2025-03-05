0b0a3df21312e78c0df4cde3a6fbcc37ea380631
Anna Kasprova <2231744@my.ipleiria.pt>
2025-03-05 10:34:56 +0000

    adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,10 +2,12 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
-
+    private JLabel lblMensagem2
+            ;
     public HelloWorld(){
         setContentPane(panel1);
         pack();
+        lblMensagem.setText("A Anna esteve aqui");
     }
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
