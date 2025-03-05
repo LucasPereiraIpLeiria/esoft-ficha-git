c6ad2b44750425d6e75bbdbab0ea920416938349
Anna Kasprova <2231744@my.ipleiria.pt>
2025-03-05 10:59:32 +0000

    merge

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
-    private JLabel lblMensagem2
-            ;
+    private JLabel lblMensagem2;
+
     public HelloWorld(){
         setContentPane(panel1);
         pack();
